e3dc6bf12088ce0fed357a67260ed827180e17b8
bugsagweler-ctrl <bugsagweler@gmail.com>
2025-10-29 12:20:49 +0200

    Add files via upload

---
+++ operation-types.js
@@ -0,0 +1,16 @@
+// TODO: Create a variable called number1 and store any number in it 
+var number1 = 32;
+// TODO: Create a variable called number2 and store a different number in it 
+var  number2 = 44;
+// TODO: Convert number1 to a string and store it in a new variable 
+let number1String = number1.toString();
+// TODO: Add number1 and number2 and store the result in a variable called sum 
+var sum = number1 + number2; 
+// TODO: Multiply number1 and number2 and store the result in a variable called product 
+var product = number1 * number2;
+// TODO: Print all results to the console
+console.log(number1);
+console.log(number2);
+console.log(number1String);
+console.log(sum);
+console.log(product);
